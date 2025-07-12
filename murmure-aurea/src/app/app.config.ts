0414ad509618158f118f3098b46a1dc8609eab37
ArletteB <arlettebello81@gmail.com>
2025-07-13 01:09:09 +0200

    uninstall lucide-angular library

--- murmure-aurea/src/app/app.config.ts
+++ murmure-aurea/src/app/app.config.ts
@@ -1,16 +1,6 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
-
-
-import {
-  Feather,
-  Crown,
-  Moon,
-  Star,
-  Heart
-} from 'lucide-angular';
 
 export const appConfig: ApplicationConfig = {
   providers: [
