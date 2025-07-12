0ba3653438de2573450a95cd15585238d41c275f
ArletteB <arlettebello81@gmail.com>
2025-07-13 01:04:33 +0200

    created footer

--- murmure-aurea/src/app/app.config.ts
+++ murmure-aurea/src/app/app.config.ts
@@ -3,6 +3,19 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
+
+import {
+  Feather,
+  Crown,
+  Moon,
+  Star,
+  Heart
+} from 'lucide-angular';
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideRouter(routes),
+
+  ]
 };
